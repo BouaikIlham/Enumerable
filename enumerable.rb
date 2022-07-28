3eb09f4732789c7a4ed5e7f94d48d015102e6603
ilham bouaik <ibouaik@gmail.com>
2022-07-28 16:39:07 +0100

    add filter method

--- enumerable.rb
+++ enumerable.rb
@@ -8,7 +8,14 @@ module MyEnumerable
 
   def any?
     return true unless block_given?
-    each {|element| return true if yield(element)}
-    false 
+
+    each { |element| return true if yield(element) }
+    false
+  end
+
+  def filter
+    enum_array = []
+    each { |element| enum_array.push(element) if yield(element) }
+    enum_array
   end
 end
